54630622326a82bfa87c99f9e5cfa2d3985afab8
Shubham Singh <69746961+poojyapitaji@users.noreply.github.com>
2024-05-22 09:04:56 +0530

    removed swager from project

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,26 +1,15 @@
 import { Router, Request, Response } from 'express';
-import config from 'config';
 
-// import swaggerDocs from '../../utils/swagger.utils';
 import authRouter from './auth.route';
 import logRouter from './logs.route';
-// import app from '../../app';
 
 const router = Router();
 
-const environment = config.get<string>('environment');
-// const port = config.get<string>('server.port');
-const isDev = environment === 'development';
-
 router.get('/', (_req: Request, res: Response) => {
-  res.redirect('/v1/docs');
+  res.redirect('/api/v1/api-docs');
 });
 
 router.use('/log', logRouter);
 router.use('/auth', authRouter);
-
-if (isDev) {
-  // swaggerDocs(app, port, 'v1');
-}
 
 export default router;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-import * as swagger from './swagger.util';
 import * as avatar from './avatar_generator.util';
 
-export { swagger, avatar };
+export { avatar };
